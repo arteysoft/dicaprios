088265829cbbf4f035d11218437c92ae5cbec89d
andy <arteysoft@gmail.com>
2022-06-29 19:58:41 -0300

    ejercicios resueltos

---
+++ ejercicios_resuletos/clase1/meyer_ivan_ej1.cs
@@ -0,0 +1,17 @@
+﻿// Captura de datos:
+
+Console.WriteLine("Ingrese nombre:");
+string? nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese auto:");
+string? auto = Console.ReadLine();
+
+Guid id = Guid.NewGuid();
+
+// Salida de datos:
+
+string datos = $"Los datos del ID {id} son: {nombre} y {auto}.\nSe almacena en outputFile.txt";
+Console.WriteLine("\n" + datos);
+
+await File.WriteAllTextAsync("outputFile.txt", $"id: {id}\nnombre: {nombre}\nauto: {auto}");
+
