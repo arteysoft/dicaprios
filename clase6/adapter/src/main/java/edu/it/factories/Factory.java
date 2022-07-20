2ee0c6588e0cea4101bc253fdb12f987f11d62d5
andy <arteysoft@gmail.com>
2022-07-20 19:14:04 -0300

    adapter fin

--- clase6/adapter/src/main/java/edu/it/factories/Factory.java
+++ clase6/adapter/src/main/java/edu/it/factories/Factory.java
@@ -1,10 +1,14 @@
 package edu.it.factories;
 
-import edu.it.services.DiscadorPotencia;
+import dialerpluspro.DialerPro;
+import edu.it.services.DiscadorPotenciaAdapter;
 import edu.it.services.ServicioDiscador;
 
 public class Factory {
 	public static ServicioDiscador crearServicioDiscador() {
-		return new ServicioDiscador(new DiscadorPotencia());
+		// return new ServicioDiscador(new DiscadorPotencia());
+		return new ServicioDiscador(
+				new DiscadorPotenciaAdapter(new DialerPro())
+				);
 	}
 }
